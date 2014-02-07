ab7fbca34ec1511509d8a350a2112a154a7f874f
Eliot <ecsit39@gmail.com>
2014-02-07 15:49:06 -0500

    touch stuff

---
+++ Assets/Scripts/ExTouchObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ExTouch
+{
+    public class ExTouchObject : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public virtual bool OnTap(ExGestureObject g)
+        {
+            return false;
+        }
+
+        public virtual bool OnDrag(ExGestureObject g)
+        {
+            return false;
+        }
+
+        public virtual bool OnPull(ExGestureObject g)
+        {
+            return false;
+        }
+
+        public virtual bool OnHold(ExGestureObject g)
+        {
+            return false;
+        }
+
+        public virtual bool OnDown(ExGestureObject g)
+        {
+            return false;
+        }
+
+        public virtual bool OnPinch(ExGestureObject g)
+        {
+            return false;
+        }
+    }
+}
