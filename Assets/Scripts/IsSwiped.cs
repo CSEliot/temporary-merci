ed0644fca9d17d5d7aad78b4225178f8be04cced
Austin Chandler <achandl1@umbc.edu>
2014-03-07 14:08:32 -0500

    added alternate slice detection script

---
+++ Assets/Scripts/IsSwiped.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsSwiped : MonoBehaviour {
+
+	private bool inTopHalf;
+	private bool inBotHalf;
+	private bool buttonPressed;
+	private float mouseY;
+	private float halfScreen;
+	private bool swiped;
+
+	// Use this for initialization
+	void Start () {
+		inTopHalf = false;
+		inBotHalf = false;
+		buttonPressed = false;
+		halfScreen = Screen.height / 2;
+		swiped = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		buttonPressed = Input.GetMouseButton (0);
+		if (buttonPressed) {
+			mouseY = Input.mousePosition.y;
+			if (mouseY > halfScreen) {
+				inTopHalf = true;
+			}//end if
+			else if(mouseY < halfScreen){
+				inBotHalf = true;
+			}//end else if
+		}//end if
+		else {
+			inTopHalf = false;
+			inBotHalf = false;
+		}//end else
+		swiped = (inTopHalf && inBotHalf);
+		if (swiped) {
+			Debug.Log ("sliced");
+			inTopHalf = false;
+			inBotHalf = false;
+		}//end if
+	}
+}
